ac714cd756b20447f6d5e6ccc736aaea0c1cce93
jiangqianghua <jiangqianghua@jiangqianghuadeMacBook-Pro.local>
2018-05-26 16:57:04 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+//app.js
+App({
+  serverUrl:"http://192.168.1.101:8081",
+  userInfo:null,
+})
---
+++ pages/regist/regist.js
@@ -0,0 +1,112 @@
+// pages/regist/regist.js
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  doRegist:function(e){
+    var fromObject = e.detail.value;
+    var username = fromObject.username;
+    var password = fromObject.password;
+    if(username.length == 0 ||
+    password.length == 0){
+      wx.showToast({
+        title: '用户名或密码不能为空',
+        icon:'none',
+        duration:3000
+      })
+    }else{
+      var serverUrl = app.serverUrl ;
+      wx.request({
+        url: serverUrl+'/regist',
+        method:'POST' ,
+        data:{
+          username:username,
+          password:password
+        },
+        header:{
+          'content-type':'application/json'
+        },
+        success:function(res){
+          var status = res.data.status;
+          if(status == 200){
+            wx.showToast({
+              title: '用户注册成功!!!',
+              icon: 'none',
+              duration: 3000
+            })
+            debugger;
+            app.userInfo = res.data.data;
+          } else if(status == 500){
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'none',
+              duration: 3000
+            })
+          }
+        }
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
